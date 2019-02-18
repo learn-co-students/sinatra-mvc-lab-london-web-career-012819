36a99bbcd695d29fe9f79143c425ae1a32cd0fc9
stubrew24 <sewelly@gmail.com>
2019-02-18 17:46:58 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,15 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
   get '/' do
     erb :user_input
   end
 
   post '/piglatinize' do
     pl = PigLatinizer.new
-    @piglatin = pl.piglatinize(params[:user_phrase])
-    erb :results
+    @piglatinized = pl.piglatinize(params[:user_phrase])
+    erb :piglatinize
   end
 
 end
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,33 +1,25 @@
 class PigLatinizer
 
-  def piglatinize(input_str)
-    input_str.split(" ").length == 1 ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
-  end
+  attr_accessor :words
 
-  def consonant?(char)
-    !char.match(/[aAeEiIoOuU]/)
-  end
-
-  def piglatinize_word(word)
-    # word starts with vowel
-    if !consonant?(word[0])
-      word = word + "w"
-    # word starts with 3 consonants
-    elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
-      word = word.slice(3..-1) + word.slice(0,3)
-    # word starts with 2 consonants
-    elsif consonant?(word[0]) && consonant?(word[1])
-      word = word.slice(2..-1) + word.slice(0,2)
-    # word starts with 1 consonant
-    else
-      word = word.slice(1..-1) + word.slice(0)
+  def piglatinize(words)
+    arr = words.split
+    new_arr = []
+    arr.each do |word|
+      if word[0].downcase.match(/a|e|i|o|u/).nil? && word[1].downcase.match(/a|e|i|o|u/).nil? && word[2].downcase.match(/a|e|i|o|u/).nil?
+          new_arr << word[3..-1]+ word[0..2] + "ay"
+      elsif word[0].downcase.match(/a|e|i|o|u/).nil? && word[1].downcase.match(/a|e|i|o|u/).nil?
+          new_arr << word[2..-1]+ word[0..1] + "ay"
+      elsif word[0].downcase.match(/a|e|i|o|u/).nil?
+          new_arr << word[1..-1] + word[0] + "ay"
+      elsif word[0].downcase.match(/a|e|i|o|u/)
+          new_arr << word + "way"
+      end
     end
-    word << "ay"
+    if new_arr.length > 1
+    new_arr.join(" ")
+    else new_arr[0]
+    end
   end
-
-  def piglatinize_sentence(sentence)
-    sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
-  end
-
 
 end
